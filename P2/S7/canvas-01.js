e1fac35a10ebdea00cfd492857be63f4930cb5f6
carlosgomezcasar <ca.gomezcasar@gmail.com>
2022-03-08 09:40:33 +0100

    canvas

--- P2/S7/canvas-01.js
+++ P2/S7/canvas-01.js
@@ -5,3 +5,27 @@ const canvas = document.getElementById("canvas");
 //-- Definir el tamaño del canvas
 canvas.width = 300;
 canvas.height = 100;
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+
+ctx.beginPath();
+    //-- Definir un rectángulo de dimensiones 100x50,
+    //-- cuya esquina superior izquierda está en (5,5)
+    ctx.rect(5,5, 100, 50);
+
+    //-- Dibujar
+    ctx.fillStyle = 'red';
+
+    //-- Cambiar el tamaño de la línea del trazo
+    ctx.lineWidth = 4;
+
+    //-- Rellenar
+    ctx.fill();
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+    
+ctx.closePath()
+
