8bf0b0f1eb263f4ccc6ebf1d4f16a2cd582ebed7
carlosgomezcasar <ca.gomezcasar@gmail.com>
2022-03-10 10:21:50 +0100

    calc0-02

--- P2/S6/calc0-02.js
+++ P2/S6/calc0-02.js
@@ -1,33 +1,33 @@
 console.log("Ejecutando JS...");
 
-
-//-- Elementos de la interfaz de la calculadora
 display = document.getElementById("display")
-boton1 = document.getElementById("boton1")
-boton2 = document.getElementById("boton2")
 suma = document.getElementById("suma")
 igual = document.getElementById("igual")
 clear = document.getElementById("clear")
 
-//-- Funciones de retrollamada de los botones
-//-- Cada vez que se aprieta un boton se actua
-//-- sobre la cadena: añadiendo digito, operador +
-//-- poniendo a cero o evaluando la expresión
+//-- Obtener una colección con todos los elementos
+//-- de la clase dígito
+digitos = document.getElementsByClassName("digito")
 
-// -- Digito 1
-boton1.onclick = (ev) => {
-    display.innerHTML += ev.target.value;
-  }
-  
-  //-- Digito 2
-  boton2.onclick = (ev) => {
-    display.innerHTML += ev.target.value;
-  }
-  
-  //-- Simbolo de sumar
-  suma.onclick = (ev) => {
-    display.innerHTML += ev.target.value;
-  }
+//-- Establecer la misma función de retrollamada
+//-- para todos los botones de tipo dígito
+for (let boton of digitos) {
+
+    //-- Se ejecuta cuando se pulsa un boton
+    //-- que es un dígito
+    boton.onclick = (ev) => {
+        display.innerHTML += ev.target.value;
+        console.log("DIGITO!!!");
+    }
+}
+
+//-------- Resto de funciones de retrollamada
+
+//-- Insertar simbolo de sumar
+suma.onclick = (ev) => {
+  display.innerHTML += ev.target.value;
+}
+
 //-- Evaluar la expresion
 igual.onclick = () => {
   display.innerHTML = eval(display.innerHTML);
